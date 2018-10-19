8767d015589c5af7631e5d6ea09d822bfa64648f
Devg70 <drugove.tomsk@gmail.com>
2018-10-19 20:23:44 +0700

    Merge branch 'master' of github.com:test-qeep/python-first-project
    # Conflicts:
    #	.gitignore
    #	Vagrantfile.dist
    #	requirements.txt
    #	setup.yml

---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+def generate_links():
+    with app.test_request_context():
+        misha_the_great_link = url_for(
+            'hello_user',
+            username='Misha The Great'
+        )
+        danil_the_great_link = url_for(
+            'hello_user',
+            username='Danil The Great'
+        )
+        index_link = url_for('index')
+
+        links = (
+            ("Misha's page", misha_the_great_link),
+            ("Danil's page", danil_the_great_link),
+            ('Index', index_link),
+        )
+
+    return links
+
+
+@app.route('/')
+def index():
+    links = generate_links()
+    return render_template('index.html', links=links)
+
+
+@app.route('/user/')
+@app.route('/user/<username>')
+def hello_user(username=None):
+    links = generate_links()
+    return render_template(
+        'user.html',
+        username=username,
+        links=links,
+    )
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
