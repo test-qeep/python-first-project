a09500d5fd502896d09d32b3ba56eb79ac9afaf6
Devg70 <drugove.tomsk@gmail.com>
2018-10-25 17:44:17 +0700

    Changed tuples to dictionaries in links list

--- server.py
+++ server.py
@@ -14,12 +14,18 @@ def generate_links():
             username='Danil The Great'
         )
         index_link = url_for('index')
-
-        links = (
-            ("Misha's page", misha_the_great_link),
-            ("Danil's page", danil_the_great_link),
-            ('Index', index_link),
+        index_with_params_link = url_for(
+            'index',
+            param1='param1',
+            param2='param2'
         )
+
+        links = {
+            "Misha's page": misha_the_great_link,
+            "Danil's page": danil_the_great_link,
+            'Index': index_link,
+            'Index with params': index_with_params_link,
+        }
 
     return links
 
