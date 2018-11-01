7936de57cc33c6274cdd2075ed86ed11ec8ee9a0
Devg70 <drugove.tomsk@gmail.com>
2018-11-01 20:06:19 +0700

    Added cards with info from slides

--- server.py
+++ server.py
@@ -33,17 +33,32 @@ def generate_links():
 @app.route('/')
 def index():
     links = generate_links()
-    images = [
+    slides = [
         {
-            'src': 'https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg',
-            'title': 'Image title',
+            'image_src': 'https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg',
+            'image_title': 'Image title',
+            'heading': 'Отдохните на пляже в Таиланде',
+            'subheading': 'Только у нас вы можете заказать билет по самой низкой цене',
         },
         {
-            'src': 'https://bipbap.ru/wp-content/uploads/2017/04/3-8.jpg',
-            'title': 'Image 2 title',
-        }
+            'image_src': 'https://bipbap.ru/wp-content/uploads/2017/04/3-8.jpg',
+            'image_title': 'Image 2 title',
+            'heading': 'Удивительная природа',
+        },
+        {
+            'image_src': 'https://fullpicture.ru/wp-content/uploads/2016/09/africaplaces41.jpg',
+            'image_title': 'Озеро в Африке',
+            'heading': 'Живописные места в Африке',
+            'subheading': 'Красивое озеро в Африке',
+        },
+        {
+            'image_src': 'https://fullpicture.ru/wp-content/uploads/2016/09/africaplaces1.jpg',
+            'image_title': 'Водопад Виктория',
+            'heading': 'Живописные места в Африке',
+            'subheading': 'Самый крупный водопад в мире: водопад Виктория',
+        },
     ]
-    return render_template('index.html', links=links, images=images)
+    return render_template('index.html', links=links, slides=slides)
 
 
 @app.route('/user/')
