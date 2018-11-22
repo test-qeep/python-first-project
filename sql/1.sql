680783aa5e93683d3e8d487a44300fded9748881
Devg70 <drugove.tomsk@gmail.com>
2018-11-22 20:28:55 +0700

    Added dump Fixed migration

--- sql/1.sql
+++ sql/1.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `shop`.`product` (
+CREATE TABLE `product` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NULL,
   `price` INT UNSIGNED NULL,
@@ -6,36 +6,36 @@ CREATE TABLE `shop`.`product` (
   `category_id` INT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE `shop`.`category` (
+CREATE TABLE `category` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NULL,
   `image_url` VARCHAR(255) NULL,
   `description` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
 
-ALTER TABLE `shop`.`product`
+ALTER TABLE `product`
 ADD INDEX `fk_category_idx` (`category_id` ASC);
 ;
-ALTER TABLE `shop`.`product`
+ALTER TABLE `product`
 ADD CONSTRAINT `fk_category`
   FOREIGN KEY (`category_id`)
-  REFERENCES `shop`.`category` (`id`)
+  REFERENCES `category` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-CREATE TABLE `shop`.`image` (
+CREATE TABLE `image` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(60) NULL,
   `url` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`));
 
-ALTER TABLE `shop`.`image`
+ALTER TABLE `image`
 ADD COLUMN `product_id` INT NULL AFTER `url`,
 ADD INDEX `fk_product_idx` (`product_id` ASC);
 ;
-ALTER TABLE `shop`.`image`
+ALTER TABLE `image`
 ADD CONSTRAINT `fk_product`
   FOREIGN KEY (`product_id`)
-  REFERENCES `shop`.`product` (`id`)
+  REFERENCES `product` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
