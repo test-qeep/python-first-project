b35db79665fd54281f2ebf408d910a95c4b161e0
Devg70 <drugove.tomsk@gmail.com>
2018-12-20 20:09:47 +0700

    Extracted DB-related code to repository file

---
+++ repository/shop.py
@@ -0,0 +1,62 @@
+import pymysql.cursors
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='123123',
+                             db='shop',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+def find_category(category_id):
+    with connection.cursor() as cursor:
+        cursor.execute('''
+        SELECT
+            id,
+            title,
+            description,
+            image_url
+        FROM `category`
+        WHERE id = %s
+        ''', (category_id,))
+        category = cursor.fetchone()
+
+    return category
+
+
+def find_all_categories():
+    with connection.cursor() as cursor:
+        sql = '''
+        SELECT
+            id,
+            title,
+            description,
+            image_url
+        FROM category
+        '''
+        cursor.execute(sql)
+        categories = cursor.fetchall()
+
+    return categories
+
+
+def find_products_by_category(category_id):
+    with connection.cursor() as cursor:
+        sql = '''
+        SELECT
+            p.id,
+            p.title,
+            description,
+            price,
+            category_id,
+            i.id AS image_id,
+            i.title AS image_title,
+            i.url AS image_url
+        FROM product p
+        LEFT JOIN image i ON product.id = i.product_id
+        WHERE category_id = %s
+        '''
+        cursor.execute(sql, (category_id,))
+        products = cursor.fetchall()
+
+    return products
--- server.py
+++ server.py
@@ -1,14 +1,8 @@
 from flask import Flask, url_for, render_template
-import pymysql.cursors
+
+from repository import shop
 
 app = Flask(__name__)
-
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='123123',
-                             db='shop',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
 
 
 def generate_links():
@@ -42,44 +36,18 @@ def generate_links():
 def index():
     links = generate_links()
 
-    with connection.cursor() as cursor:
-        sql = '''
-        SELECT
-            id AS category_id,
-            title AS heading,
-            title AS image_title,
-            description AS subheading,
-            image_url AS image_src
-        FROM category
-        '''
-        cursor.execute(sql)
-        slides = cursor.fetchall()
+    categories = shop.find_all_categories()
+    slides = list()
+    for x in categories:
+        slide = {
+            'category_id': x.get('id'),
+            'heading': x.get('title'),
+            'image_title': x.get('title'),
+            'subheading': x.get('description'),
+            'image_src': x.get('image_url')
+        }
+        slides.append(slide)
 
-    # slides = [
-    #     {
-    #         'image_src': 'https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg',
-    #         'image_title': 'Image title',
-    #         'heading': 'Отдохните на пляже в Таиланде',
-    #         'subheading': 'Только у нас вы можете заказать билет по самой низкой цене',
-    #     },
-    #     {
-    #         'image_src': 'https://bipbap.ru/wp-content/uploads/2017/04/3-8.jpg',
-    #         'image_title': 'Image 2 title',
-    #         'heading': 'Удивительная природа',
-    #     },
-    #     {
-    #         'image_src': 'https://fullpicture.ru/wp-content/uploads/2016/09/africaplaces41.jpg',
-    #         'image_title': 'Озеро в Африке',
-    #         'heading': 'Живописные места в Африке',
-    #         'subheading': 'Красивое озеро в Африке',
-    #     },
-    #     {
-    #         'image_src': 'https://fullpicture.ru/wp-content/uploads/2016/09/africaplaces1.jpg',
-    #         'image_title': 'Водопад Виктория',
-    #         'heading': 'Живописные места в Африке',
-    #         'subheading': 'Самый крупный водопад в мире: водопад Виктория',
-    #     },
-    # ]
     return render_template('index.html', links=links, slides=slides)
 
 
@@ -93,38 +61,19 @@ def hello_user(username=None):
         links=links,
     )
 
+
 @app.route('/category/<int:category_id>')
 def category_page(category_id):
     links = generate_links()
 
-    with connection.cursor() as cursor:
-        sql = '''
-        SELECT
-            p.title,
-            description,
-            price,
-            i.title AS image_title,
-            url AS image_url
-        FROM product p 
-        LEFT JOIN image i
-        ON p.id = i.product_id
-        WHERE category_id = %s        
-        '''
-        cursor.execute(sql, (category_id,))
-        products = cursor.fetchall()
-
-        cursor.execute('''
-        SELECT title
-        FROM category
-        WHERE id = %s
-        ''', (category_id,))
-        category = cursor.fetchone()
+    category = shop.find_category(category_id)
+    products = shop.find_products_by_category(category_id)
 
     return render_template(
         'category.html',
         products=products,
         category=category,
-        links=links,
+        links=links
     )
 
 
