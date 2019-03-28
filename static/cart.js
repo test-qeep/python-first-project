1ba6bf5c27dee62f215e8694aab7d848e8aa6dce
Mikhail <test@qeep.pro>
2019-03-28 11:29:40 +0000

    Refactored cart code to generate string with html code for cart table

--- static/cart.js
+++ static/cart.js
@@ -1,33 +1,45 @@
 const decrement = a => parseInt(a) ? a - 1 : 0
 const increment = a => parseInt(a) + 1
 
-const cartPositions = Array.from(document.querySelectorAll('.cart-position'))
+const cart = {
+    deliveryPrice: 120,
+    deliveryMethod: 'post',
+    positions: [
+        {id: 1, title: 'Шорты 2', price: 1200, amount: 2, sum: 0},
+        {id: 3, title: 'Шорты 3', price: 1200, amount: 3, sum: 0},
+        {id: 4, title: 'Шорты 4', price: 1200, amount: 1, sum: 0},
+    ]
+}
 
-cartPositions.forEach((cartPosition) => {
-    const price = parseInt(cartPosition.querySelector('.price').innerHTML)
-    const amount = cartPosition.querySelector('.amount')
+const generateCartTable = (cart) => {
+    const priceTd = document.createElement('td')
+    priceTd.innerHTML = '100'
 
-    const plusButton = cartPosition.querySelector('.plus-button')
-    const minusButton = cartPosition.querySelector('.minus-button')
+    const trs = cart.positions.map(position => {
+        position.sum = position.price * position.amount
 
-    const sum = cartPosition.querySelector('.sum')
-    sum.innerHTML = parseInt(amount.value) * price
-
-    const total = cartPosition.querySelector('.total')
-
-    plusButton.addEventListener('click', () => {
-        const amountValue = increment(amount.value)
-        sum.innerHTML = amountValue * price
-        amount.value = amountValue
-
+        return `
+        <tr class="cart-position">
+            <td>${position.title}</td>
+            <td class="price">${position.price}</td>
+            <td>
+                <button class="minus-button">-</button>
+                <input type="text" value="${position.amount}" class="amount">
+                <button class="plus-button">+</button>
+            </td>
+            <td class="sum">${position.sum}</td>
+        </tr>
+        `
     })
 
-    minusButton.addEventListener('click', () => {
-        const amountValue = decrement(amount.value)
-        sum.innerHTML = amountValue * price
-        amount.value = amountValue
-    })
-})
+    const totalTr = `
+        <tr>
+            <td colspan="3"></td>
+            <td class="total">0</td>
+        </tr>`
+
+    return `<table>${trs}${totalTr}</table>`
+}
 
 /*
 
